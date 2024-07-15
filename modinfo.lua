4c3cc3cd6745982eaa74f735c25566aff7c4b148
Cliffford W. <55302963+ClifffordW@users.noreply.github.com>
2024-07-15 21:37:25 +0200

    Lua

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
-name = "Hot Reload"
+name = "Hot Reload LuaJit"
 author = "Jerry"
-description = "simple hot fix, will cause slower loading and more memory usage, and many be have bug"
+description = "Combines Hot Reload and LuaJit into one mod. Requires LuaJit installed. Comes with handy indicator"
 
 version = "1.0.0"
 forumthread = ""
@@ -9,15 +9,75 @@ api_version = 6
 api_version = 10
 
 dst_compatible = true
-all_clients_require_mod = true
--- client_only_mod = true
+all_clients_require_mod = false
+client_only_mod = true
 
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
 priority = 1.7976931348623157e+306
 
-server_filter_tags = {
-}
+server_filter_tags = {}
 
-configuration_options = {}
+local lc = locale
+local function translate(t)
+	t.zhr = t.zh
+	t.zht = t.zht or t.zh
+	return t[lc] or t.en
+end
+
+configuration_options = {
+	{
+		name = "skipanim",
+		label = translate({ en = "Load in Animation", zh = "加载动画" }),
+		hover = translate({
+			en = "Turn off if you want to skip the ingame showing animation",
+			zh = "如果您想跳过游戏内显示动画，请关闭",
+		}),
+		options = {
+			{ description = "On", data = true },
+			{ description = "Off", data = false },
+		},
+		default = true,
+	},
+
+	{
+		name = "EnabledJIT",
+		label = translate({ en = "EnabledJIT", zh = "开启JIT模式" }),
+		hover = translate({
+			en = "It is recommended to turn off if the lag is severe in the game",
+			zh = "在游戏中卡顿现象很严重的建议关闭",
+		}),
+		options = {
+			{ description = "On", data = true },
+			{ description = "Off", data = false },
+		},
+		default = true,
+	},
+	{
+		name = "JitOpt",
+		label = translate({ en = "JitOpt", zh = "JIT优化选项" }),
+		hover = translate({
+			en = "maybe faster, maybe slower.",
+			zh = "可能更快, 可能更慢.",
+		}),
+		options = {
+			{ description = "On", data = true },
+			{ description = "Off", data = false },
+		},
+		default = true,
+	},
+	{
+		name = "ModBlackList",
+		label = translate({ en = "ModJitBlackList", zh = "MODJit黑名单" }),
+		hover = translate({
+			en = "some mod is't not appropriate",
+			zh = "有些mod可能写的特别,不合适jit模式",
+		}),
+		options = {
+			{ description = "On", data = true },
+			{ description = "Off", data = false },
+		},
+		default = true,
+	},
+}
