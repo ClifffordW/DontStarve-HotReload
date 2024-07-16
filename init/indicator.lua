e7d03bc715fed636b50a9ad067f261014734a02c
Cliffford W. <55302963+ClifffordW@users.noreply.github.com>
2024-07-16 15:48:45 +0200

    Hotrel

---
+++ init/indicator.lua
@@ -0,0 +1,109 @@
+local skipanimation = GetModConfigData("skipanim")
+
+local hotreloadicon
+
+
+_G.staticScheduler:ExecuteInTime(0, function()
+	if _G.TheFrontEnd and not hotreload_text then
+		local Text = require("widgets/text")
+		local Image = require("widgets/image")
+
+		local LOC = require("languages/loc")
+		local locale = LOC.GetLocaleCode()
+
+		local hotreload_lang = {
+			en = "HOT RELOAD",
+			es = "RECARGA RÁPIDA",
+			ru = "ГОРЯЧАЯ ПЕРЕЗАГРУЗКА",
+			fr = "RECHARGEMENT À CHAUD",
+			de = "HEISSER NEUSTART",
+			it = "RICARICA VELOCE",
+			ja = "ホットリロード",
+			ko = "핫 리로드",
+			pt = "RECARGA RÁPIDA",
+			pl = "GORĄCE PRZEŁADOWANIE",
+			zh = "热重载",
+			zht = "热重载",
+		}
+
+		local hotreloadtext = hotreload_lang[LOC.GetLocaleCode()] or hotreload_lang.en
+
+		local ingame = _G.InGamePlay()
+
+		hotreloadicon = _G.TheFrontEnd.overlayroot:AddChild(Image("images/hot_reload.xml", "HotReload_Whaskal.tex"))
+		hotreloadicon:SetPosition(1000, 500)
+		hotreloadicon:SetScale(0.15, 0.15)
+		hotreloadicon:Hide()
+
+		local hotreloadicon_shadow = hotreloadicon:AddChild(Image("images/hot_reload.xml", "HotReload_Whaskal.tex"))
+		hotreloadicon_shadow:SetTint(0 / 255, 0 / 255, 0 / 255, 1)
+		hotreloadicon_shadow:SetScale(1.05, 1.05)
+
+		hotreloadicon_shadow:MoveToBack()
+
+		local hotreload_text = hotreloadicon:AddChild(Text(_G.UIFONT, 200))
+		hotreload_text:SetPosition(0, -300)
+		hotreload_text:SetHAlign(_G.ANCHOR_MIDDLE)
+		hotreload_text:SetVAlign(_G.ANCHOR_TOP)
+		hotreload_text:SetString(hotreloadtext)
+		hotreload_text:SetColour(_G.RGB(255, 150, 0))
+		hotreload_text:SetScale(0)
+
+		local hotreload_text_shadow = hotreload_text:AddChild(Text(_G.UIFONT, 200))
+		hotreload_text_shadow:SetPosition(0, 0)
+		hotreload_text_shadow:SetHAlign(_G.ANCHOR_MIDDLE)
+		hotreload_text_shadow:SetVAlign(_G.ANCHOR_TOP)
+		hotreload_text_shadow:SetString(hotreloadtext)
+		hotreload_text_shadow:SetColour(_G.RGB(0, 0, 0))
+
+		hotreload_text_shadow:SetScale(1.05, 1.05)
+		hotreload_text_shadow:MoveToBack()
+
+		if not _G.InGamePlay() then
+			hotreloadicon:Show()
+
+			hotreload_text:Hide()
+			hotreloadicon:SetPosition(50, 1000)
+		end
+
+		if skipanimation and ingame then
+			hotreloadicon:SetPosition(200, 80)
+			hotreloadicon:Show()
+
+			hotreload_text:SetScale(1)
+		end
+
+		TheFrontEnd.overlayroot.inst:DoTaskInTime(1.85, function()
+			if skipanimation or not ingame then
+				return
+			end
+
+			TheFrontEnd.overlayroot.inst:DoTaskInTime(0, function()
+				hotreloadicon:Show()
+			end)
+
+			hotreloadicon:TintTo({ r = 0, g = 0, b = 0, a = 0 }, { r = 1, g = 1, b = 1, a = 1 }, 1.5 * 2)
+
+			hotreloadicon:ScaleTo(0, 0.5, 0.85, function()
+				hotreloadicon:ScaleTo(0.5, 0.15, 0.25)
+			end)
+
+			hotreloadicon:RotateTo(0, 360, 1.5)
+
+			TheFrontEnd.overlayroot.inst:DoTaskInTime(1.85, function()
+				hotreload_text:ScaleTo(0, 1, 0.25)
+				hotreloadicon:MoveTo({ x = 1000, y = 500, z = 0 }, { x = 200, y = 80, z = 0 }, 0.85)
+			end)
+		end)
+	end
+
+	if not jit then
+		_G.staticScheduler:ExecuteInTime(0.5, function()
+			hotreloadicon:SetTint(255 / 255, 0 / 255, 0 / 255, 1)
+		end)
+	end
+
+
+end)
+
+
--- modinfo.lua
+++ modinfo.lua
@@ -1,24 +1,3 @@
-name = "Hot Reload LuaJit"
-author = "Jerry"
-description = "Combines Hot Reload and LuaJit into one mod. Requires LuaJit installed. Comes with handy indicator"
-
-version = "1.0.0"
-forumthread = ""
-
-api_version = 6
-api_version = 10
-
-dst_compatible = true
-all_clients_require_mod = false
-client_only_mod = true
-
-icon_atlas = "modicon.xml"
-icon = "modicon.tex"
-
-priority = 1.7976931348623157e+306
-
-server_filter_tags = {}
-
 local lc = locale
 local function translate(t)
 	t.zhr = t.zh
@@ -26,58 +5,225 @@ local function translate(t)
 	return t[lc] or t.en
 end
 
-configuration_options = {
-	{
-		name = "skipanim",
-		label = translate({ en = "Load in Animation", zh = "加载动画" }),
-		hover = translate({
-			en = "Turn off if you want to skip the ingame showing animation",
-			zh = "如果您想跳过游戏内显示动画，请关闭",
-		}),
-		options = {
-			{ description = "On", data = true },
-			{ description = "Off", data = false },
-		},
-		default = true,
-	},
 
-	{
-		name = "EnabledJIT",
-		label = translate({ en = "EnabledJIT", zh = "开启JIT模式" }),
-		hover = translate({
-			en = "It is recommended to turn off if the lag is severe in the game",
-			zh = "在游戏中卡顿现象很严重的建议关闭",
-		}),
-		options = {
-			{ description = "On", data = true },
-			{ description = "Off", data = false },
-		},
-		default = true,
-	},
-	{
-		name = "JitOpt",
-		label = translate({ en = "JitOpt", zh = "JIT优化选项" }),
-		hover = translate({
-			en = "maybe faster, maybe slower.",
-			zh = "可能更快, 可能更慢.",
-		}),
-		options = {
-			{ description = "On", data = true },
-			{ description = "Off", data = false },
-		},
-		default = true,
-	},
-	{
-		name = "ModBlackList",
-		label = translate({ en = "ModJitBlackList", zh = "MODJit黑名单" }),
-		hover = translate({
-			en = "some mod is't not appropriate",
-			zh = "有些mod可能写的特别,不合适jit模式",
-		}),
-		options = {
-			{ description = "On", data = true },
-			{ description = "Off", data = false },
-		},
-		default = true,
-	},
+
+--Beta, Final, or leave for no name
+versiontype = ""
+name = "Hot Reload (LuaJIT)"
+
+author = "Jerry"
+
+version = "1.0"
+
+config = true
+Language = locale
+
+contributors = ""
+write_contributors = false
+credits_only = false
+
+main_icon = "modicon"
+
+priority = 1.7976931348623157e+306
+
+api_version = 10
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+all_clients_require_mod = false
+client_only_mod = true
+server_only_mod = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+local scales = {}
+
+for i = 1, 20 do
+	scales[i] = { description = "x" .. i / 10, data = i / 10 }
+end
+
+local pos = {
+	[1] = { description = "Default", data = 0 },
 }
+
+for i = 2, 15 do
+	pos[i] = { description = "+" .. i .. "0", data = i * 10 }
+end
+
+local opt_Empty = { { description = "", data = 0 } }
+local function Title(title, hover)
+	return {
+		name = title,
+		hover = hover,
+		options = opt_Empty,
+		default = 0,
+	}
+end
+
+local SEPARATOR = Title("")
+
+modinfo_ver = "2.2"
+if config == true then
+	configuration_options = {
+		{
+			name = "skipanim",
+			label = translate({ en = "Load in Animation", zh = "加载动画" }),
+			hover = translate({
+				en = "Turn off if you want to skip the ingame showing animation",
+				zh = "如果您想跳过游戏内显示动画，请关闭",
+			}),
+			options = {
+				{ description = "On", data = true },
+				{ description = "Off", data = false },
+			},
+			default = true,
+		},
+
+
+		
+	}
+end
+
+icon_atlas = main_icon .. ".xml"
+icon = main_icon .. ".tex"
+
+versiontypes = {
+	final = "[Final]",
+	beta = "[Beta]",
+	disc = "[Discontinued]",
+	redux = "[Redux]",
+}
+versiontype = versiontypes[versiontype] or ""
+
+modinfo_ver = modinfo_ver
+
+if versiontype == "" then
+	name = name
+else
+	name = name .. " \n" .. versiontype .. ""
+end
+
+folder_name = folder_name or "workshop-"
+if not folder_name:find("workshop-") then
+	name = name .. " - GitHub Ver."
+end
+
+old_author = author
+if contributors == "" or contributors == nil then
+	author = author
+elseif write_contributors == true then
+	author = author .. " and " .. " " .. contributors
+end
+
+-- Description Components
+versiontypes = {
+	final = "[Final]",
+	beta = "[Beta]",
+	disc = "[Discontinued]",
+	redux = "[Redux]",
+}
+versiontype = versiontypes[versiontype] or ""
+
+modinfo_ver = modinfo_ver
+
+if versiontype == "" then
+	name = name
+else
+	name = name .. " \n" .. versiontype .. ""
+end
+
+folder_name = folder_name or "workshop-"
+if not folder_name:find("workshop-") then
+	name = name .. " - GitHub Ver."
+end
+
+old_author = author
+if contributors == "" or contributors == nil then
+	author = author
+elseif write_contributors == true then
+	author = author .. " and " .. " " .. contributors
+end
+
+-- Description Components
+local descriptions = {
+	en = {
+		name = "Hot Reload",
+		desc = [[This mod allows you to save code you are working on, updating it in real time. Documentation will be available]],
+		changelog = [[󰀏 What's New:
+
+		󰀈 Hot Reload
+
+		]],
+		credits = "󰀭 Credits: " .. contributors,
+		mark2 = "󰀩 Modinfo Version: " .. modinfo_ver
+	},
+	zh = {
+		name = "热重载",
+		desc = [[ Insert Here Jerry ]],
+		changelog = [[󰀏 更新内容：
+
+		󰀈 热重载
+
+		]],
+		credits = "󰀭 致谢: " .. contributors,
+		mark2 = "󰀩 模组信息版本: " .. modinfo_ver
+	},
+	zht = {
+		name = "熱重載 (LuaJIT)",
+		desc = [[ Insert Here Jerry ]],
+		changelog = [[󰀏 更新內容：
+
+		󰀈 熱重載
+
+		]],
+		credits = "󰀭 致謝: " .. contributors,
+		mark2 = "󰀩 模組信息版本: " .. modinfo_ver
+	}
+}
+
+local lang = Language or "en"
+
+
+name = descriptions[lang].name
+local desc = descriptions[lang].desc
+local changelog = descriptions[lang].changelog
+local credits = descriptions[lang].credits
+local mark2 = descriptions[lang].mark2
+
+if write_contributors == true or credits_only == true and contributors ~= "" then
+	descfill = desc .. "\n\n󰀝 " .. (lang == "zh" and "模组版本: " or lang == "zht" and "模組版本: " or "Mod Version: ") .. version .. "\n" .. changelog .. "\n\n" .. credits .. "\n\n" .. mark2
+else
+	descfill = desc .. "\n\n" .. changelog .. "\n󰀝 " .. (lang == "zh" and "版本: " or lang == "zht" and "版本: " or "Version: ") .. version .. "\n\n" .. mark2
+end
+
+description = descfill
+
+
+local lang = Language or "en"
+local desc = descriptions[lang].desc
+local changelog = descriptions[lang].changelog
+local credits = descriptions[lang].credits
+local mark2 = descriptions[lang].mark2
+
+if write_contributors == true or credits_only == true and contributors ~= "" then
+	descfill = desc .. "\n\n󰀝 " .. (lang == "zh" and "模组版本: " or lang == "zht" and "模組版本: " or "Mod Version: ") .. version .. "\n" .. changelog .. "\n\n" .. credits .. "\n\n" .. mark2
+else
+	descfill = desc .. "\n\n" .. changelog .. "\n󰀝 " .. (lang == "zh" and "版本: " or lang == "zht" and "版本: " or "Version: ") .. version .. "\n\n" .. mark2
+end
+
+
+description = descfill
+
